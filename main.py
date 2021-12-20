3d03b1067caae1df16b99ff1bc1fa2aa0f360171
nerfshacopls <dsjgdavid@gmail.com>
2021-12-20 15:38:08 +0100

    Noticias ElMundo done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib import request as rq
 import datetime as dt
+import ssl
 
 import os
 from pathlib import Path
@@ -39,6 +40,18 @@ def getElMundoNews(mainUrl, categoria):
 
     return listaNoticias
 
+def getElPaisNews(mainUrl, categoria):
+    html = rq.urlopen(mainUrl, context=ssl.SSLContext()).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    listaNoticias = []
+    articulos = soup.findAll("article")
+    listaLinks = []
+    for a in articulos:
+        link = "https://elpais.com" + a.find("a")["href"]
+        listaLinks.append(link)
+    print(listaLinks)
+    #return listaNoticias
 
 def guardarNoticias(listaN: list, ruta):
     for i,n in enumerate(listaN):
@@ -46,14 +59,37 @@ def guardarNoticias(listaN: list, ruta):
         nombreArchivo = f"{n.categoria}.{fecha.year}-{fecha.month}-{fecha.day}.{i}.txt"
         print(nombreArchivo)
 
-        # Path(ruta).mkdir(parents=True, exist_ok=True)
-        os.mkdir(ruta)
-        f = open(os.path.join(ruta, nombreArchivo), "w")
-        f.write("Now the file has more content!")
+        s = "####"
+        texto = f"{n.titulo}{s}\n" \
+                f"{n.subtitulo}{s}\n" \
+                f"{n.fecha}{s}\n" \
+                f"{n.url}{s}\n" \
+                f"{n.categoria}{s}\n" \
+                f"{n.periodico}{s}\n" \
+                f"{n.tags}{s}\n" \
+                f"{n.texto}{s}\n" \
+            # Path(ruta).mkdir(parents=True, exist_ok=True)
+        cd = os.getcwd() + "/"+n.periodico
+
+        if not os.path.exists(cd):
+            os.mkdir(cd)
+        cd2 = cd + ruta
+
+        if not os.path.exists(cd2):
+            os.mkdir(cd2)
+        f = open(os.path.join(cd2, nombreArchivo), "w")
+        f.write(texto)
         f.close()
 
-saludElmundo= getElMundoNews("https://www.elmundo.es/ciencia-y-salud/salud.html", "Salud")
-# tecnologElmundo = getElMundoNews("https://www.elmundo.es/tecnologia.html", "Tecnologia")
-# cienciaElmundo = getElMundoNews("https://www.elmundo.es/ciencia-y-salud/ciencia.html", "Ciencia")
+def elMundo():
+    saludElmundo = getElMundoNews("https://www.elmundo.es/ciencia-y-salud/salud.html", "Salud")
+    guardarNoticias(saludElmundo, "/Salud/")
+    tecnologElmundo = getElMundoNews("https://www.elmundo.es/tecnologia.html", "Tecnologia")
+    cienciaElmundo = getElMundoNews("https://www.elmundo.es/ciencia-y-salud/ciencia.html", "Ciencia")
+    guardarNoticias(tecnologElmundo, "/Tecnologia/")
+    guardarNoticias(cienciaElmundo, "/Ciencia/")
 
-guardarNoticias(saludElmundo, "/Salud/")
+
+getElPaisNews("https://elpais.com/noticias/sanidad/","Sanidad")
+
+
