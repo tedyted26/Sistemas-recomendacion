976e149acc88cce8696bc04f1af9d6a08ab3c2fd
Mario Uceda <marioucedayeves@gmail.com>
2022-01-04 17:30:31 +0100

    metodo Coseno

--- tratamientoDatos.py
+++ tratamientoDatos.py
@@ -79,7 +79,6 @@ def leerFicheros(rutaFichero):
     fichero = texto.splitlines()
     return fichero
 
-
 #Metodo para generar el diccionario
 def generarDiccionario():
     diccionario = []
@@ -130,7 +129,7 @@ def generarDiccionario():
     
     return diccionario
 
-#Metodo
+#Metodo para generar la matriz
 def generarMatriz():
     #leo el diccionario
     diccionario = leerFicheros(rutaDiccionario)
@@ -182,6 +181,23 @@ def generarMatriz():
             i+=1
         numpy.savetxt(rutaMatriz,matriz,fmt='%i')
 
+#Metodo para calcular la similitud en función del coseno
+def coseno(doc1, doc2):
+    numerador = 0
+    cuadradosDoc1 = 0
+    cuadradosDoc2 = 0
+    i = 0
+    for i in range(len(doc1)):
+        numerador += (doc1[i] * doc2[i])
+        cuadradosDoc1 += doc1[i]**2
+        cuadradosDoc2 += doc2[i]**2
+    raizDoc1 = cuadradosDoc1**(0.5)
+    raizDoc2 = cuadradosDoc2**(0.5)
+    denominador = raizDoc1 * raizDoc2
+    return numerador/denominador
+
+    
 #Main
 #generarDiccionario()
-generarMatriz()
+#generarMatriz()
+print( coseno([3,6,6], [3,3,5]) )
