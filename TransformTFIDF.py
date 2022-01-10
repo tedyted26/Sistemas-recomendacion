a1eb9f1fdd49ced9e393546766cedea98f9ea73c
nerfshacopls <dsjgdavid@gmail.com>
2022-01-11 00:05:40 +0100

    TFIDF ULTRARAPIDO con numpy

--- TransformTFIDF.py
+++ TransformTFIDF.py
@@ -1,6 +1,8 @@
 import math
 
 # LEER PARA USAR:
+import numpy
+
 '''
 matrixToTFIDF: para transformar la matriz
 listToTFIDF: para transformar una lista externa
@@ -11,11 +13,13 @@ indexListToTFIDF: para transformar solo una fila de la matriz
 # Devuelve la matriz entera en TFIDF
 def matrixToTFIDF(matriz):
     new_m = []
+    listaIDF = getIDFlistOfMatriz(matriz)
     for i in range(len(matriz)):
-        new_m.append(indexListToTFIDF(matriz, i))
+        print("Posicion de lista operandose TFIDF:", i)
+        new_m.append(indexListToTFIDF(matriz, i, listaIDF))
     return new_m
 #Para transformar una fila de una matriz en TFIDF
-def indexListToTFIDF(matriz, index: int):
+def indexListToTFIDF(matriz, index: int, listaIDF: list):
     new_list = [] # Lista a devolver
 
     v = matriz[index]
@@ -24,18 +28,25 @@ def indexListToTFIDF(matriz, index: int):
         for i, w in enumerate(v):
             tf = w / n_words
 
-            w_in_docs_counter = 0
-            for row in matriz:
-                if row[i] > 0:
-                    w_in_docs_counter += 1
-
-            oper = len(matriz)/w_in_docs_counter
-
-            idf = math.log10(oper)
+            idf = listaIDF[i]
             result = tf * idf
             new_list.append(result)
     return new_list
+def getIDFlistOfMatriz(matriz):
+    lista = []
+    for i in range(len(matriz[0])):
+        print("    + Columna:", i)
+        w_in_docs_counter = 0
+        for row in matriz:
+            r = row
+            if r[i] > 0:
+                w_in_docs_counter += 1
 
+        oper = len(matriz) / w_in_docs_counter
+
+        idf = math.log10(oper)
+        lista.append(idf)
+    return lista
 # Devuelve una lista externa a la matriz en TFIDF
 def listToTFIDF(m, extVector: list):
     new_list = []  # Lista a devolver
@@ -60,20 +71,9 @@ def listToTFIDF(m, extVector: list):
 
     return new_list
 
-def testTransformations():
-    m = [[1, 0 ,0],
-         [0, 1, 0],
-         [3, 1, 1]]
-    m2 = m.copy()
-    for i in range(len(m)):
-        print(f"indexList:{indexListToTFIDF(m2, i)}")
+def testTransformMatrizToTFIDF():
+    matriz = numpy.loadtxt("matriz.txt")
+    m2 = matrixToTFIDF(matriz)
+    print()
 
-    n = [[1, 0 ,0],
-         [0, 1, 0]]
-    v = [3, 1, 1]
-    print(f"list:{listToTFIDF(n, v)}")
-
-    print(f"matrix:{matrixToTFIDF(m2)}")
-
-
-
+testTransformMatrizToTFIDF()
